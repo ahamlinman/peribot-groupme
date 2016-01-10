e598f32a9d0d39fd55764cf88542713224243bec
Alex Hamlin <alexh1995@gmail.com>
2016-01-10 14:24:23 -0500

    Clean up bot monitor spec
    Some general changes to try to make the bot monitor spec a bit cleaner.

--- spec/peribot/groupme/bot_monitor_spec.rb
+++ spec/peribot/groupme/bot_monitor_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
 describe Peribot::GroupMe::BotMonitor do
-  subject { Peribot::GroupMe::BotMonitor }
-
+  let(:instance) { Peribot::GroupMe::BotMonitor.new bot, client }
   let(:bot) { instance_double(Peribot::Bot) }
   let(:client) { instance_double(GroupMe::Client) }
-  let(:monitor) { Peribot::GroupMe::BotMonitor.new bot, client }
 
   let(:config) do
     {
@@ -15,75 +13,78 @@ describe Peribot::GroupMe::BotMonitor do
     }
   end
 
-  let(:group_list) do
-    [
-      { 'group_id' => '1', 'name' => 'Sample', 'stuff' => rand },
-      { 'group_id' => '2', 'name' => 'Another', 'stuff' => rand }
-    ]
-  end
-
   describe '.start' do
     let(:task_double) { instance_double(Concurrent::TimerTask) }
+
     it 'creates and executes a timer task' do
-      expect(subject).to receive(:from_bot).with(bot).and_return(monitor)
-      expect(monitor).to receive(:execute)
+      allow(Peribot::GroupMe::BotMonitor).to receive(:from_bot).with(bot)
+        .and_return(instance)
+      allow(instance).to receive(:execute)
+
       expect(Concurrent::TimerTask).to receive(:new).and_yield
         .and_return(task_double)
       expect(task_double).to receive(:execute)
 
-      subject.start bot
+      Peribot::GroupMe::BotMonitor.start bot
     end
 
     it 'passes keyword arguments to TimerTask' do
-      expect(Concurrent::TimerTask).to receive(:new).with(test: true)
-        .and_return(task_double)
       allow(task_double).to receive(:execute)
 
-      subject.start bot, test: true
+      expect(Concurrent::TimerTask).to receive(:new).with(test: true)
+        .and_return(task_double)
+
+      Peribot::GroupMe::BotMonitor.start bot, test: true
     end
   end
 
   describe '.from_bot' do
     it 'creates a monitor from the bot configuration' do
-      expect(bot).to receive(:config).and_return(config)
-      expect(GroupMe::Client).to receive(:new).with(token: 'TEST')
+      allow(bot).to receive(:config).and_return(config)
+      allow(GroupMe::Client).to receive(:new).with(token: 'TEST')
         .and_return(client)
-      expect(subject).to receive(:new).with(bot, client)
 
-      subject.from_bot bot
+      expect(Peribot::GroupMe::BotMonitor).to receive(:new).with(bot, client)
+
+      Peribot::GroupMe::BotMonitor.from_bot bot
     end
   end
 
   describe '#execute' do
+    let(:group_list) do
+      [
+        { 'group_id' => '1', 'name' => 'Sample', 'stuff' => rand },
+        { 'group_id' => '2', 'name' => 'Another', 'stuff' => rand }
+      ]
+    end
+
+    let(:bot_list) do
+      [
+        { 'bot_id' => '1b', 'group_id' => '1' },
+        { 'bot_id' => '2b', 'group_id' => '2' }
+      ]
+    end
+
     before(:each) do
       allow(bot).to receive(:config).and_return(config)
       allow(client).to receive(:groups).and_return(group_list)
     end
 
     context 'when a group has no bot' do
-      let(:bot_list) { [{ 'bot_id' => '1b', 'group_id' => '1' }] }
-
       it 'registers one' do
-        allow(client).to receive(:bots).and_return(bot_list)
+        allow(client).to receive(:bots).and_return(bot_list[0..0])
         expect(client).to receive(:create_bot)
 
-        subject.new(bot, client).execute
+        instance.execute
       end
     end
 
     context 'when all groups have bots' do
-      let(:bot_list) do
-        [
-          { 'bot_id' => '1b', 'group_id' => '1' },
-          { 'bot_id' => '2b', 'group_id' => '2' }
-        ]
-      end
-
       it 'does not register any' do
-        allow(client).to receive(:bots).and_return(bot_list)
+        allow(client).to receive(:bots).and_return(bot_list[0..1])
         expect(client).to_not receive(:create_bot)
 
-        subject.new(bot, client).execute
+        instance.execute
       end
     end
   end
