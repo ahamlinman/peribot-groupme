2273940554131957b0986460ca1d60323caaee70
Alex Hamlin <alexh1995@gmail.com>
2016-01-09 23:51:03 -0500

    Get rid of Bundler sample test
    It may not do anything useful yet, but it will soon.

--- spec/peribot/groupme_spec.rb
+++ spec/peribot/groupme_spec.rb
@@ -4,8 +4,4 @@ describe Peribot::GroupMe do
   it 'has a version number' do
     expect(Peribot::GroupMe::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
