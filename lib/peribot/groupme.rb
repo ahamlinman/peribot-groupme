29cb7fe34cfbe13f84feca582d7299dc52a8a597
Alex Hamlin <alexh1995@gmail.com>
2016-01-10 21:19:37 -0500

    Add .register_into method to register components
    Create a method to register all components with a given bot instance.
    This provides an easy way to integrate GroupMe support into a bot.

--- lib/peribot/groupme.rb
+++ lib/peribot/groupme.rb
@@ -16,5 +16,30 @@ module Peribot
   # this work, Peribot::GroupMe provides various components that connect that
   # plumbing to GroupMe's inputs and outputs.
   module GroupMe
+    # The interval between monitor executions, in seconds.
+    MONITOR_INTERVAL = 15
+
+    # The interval after which monitor tasks are considered to have failed, in
+    # seconds.
+    MONITOR_TIMEOUT = 15
+
+    module_function
+
+    # Register GroupMe components with the given bot. This sets up the bot for
+    # communication with GroupMe.
+    def register_into(bot)
+      bot.sender.register Peribot::GroupMe::Sender
+      bot.sender.register Peribot::GroupMe::LikeSender
+
+      Peribot::GroupMe::BotMonitor.start(
+        bot,
+        execution_interval: MONITOR_INTERVAL,
+        timeout_interval: MONITOR_TIMEOUT)
+
+      Peribot::GroupMe::WelcomeMonitor.start(
+        bot,
+        execution_interval: MONITOR_INTERVAL,
+        timeout_interval: MONITOR_TIMEOUT)
+    end
   end
 end
--- spec/peribot/groupme_spec.rb
+++ spec/peribot/groupme_spec.rb
@@ -4,4 +4,24 @@ describe Peribot::GroupMe do
   it 'has a version number' do
     expect(Peribot::GroupMe::VERSION).not_to be nil
   end
+
+  describe '.register_into' do
+    let(:bot) { instance_double(Peribot::Bot) }
+    let(:chain) { instance_double(Peribot::Middleware::Chain) }
+
+    before(:each) do
+      allow(bot).to receive(:sender).and_return(chain)
+    end
+
+    it 'registers tasks and starts monitors' do
+      expect(bot.sender).to receive(:register).with(Peribot::GroupMe::Sender)
+      expect(bot.sender).to receive(:register)
+        .with(Peribot::GroupMe::LikeSender)
+
+      expect(Peribot::GroupMe::BotMonitor).to receive(:start)
+      expect(Peribot::GroupMe::WelcomeMonitor).to receive(:start)
+
+      Peribot::GroupMe.register_into bot
+    end
+  end
 end
