16664b0dc2bca3c6596e71abd5d1f881aad8565a
Alex Hamlin <alexh1995@gmail.com>
2016-01-23 04:57:05 -0500

    Remove monitors from peribot-groupme
    Remove the bot monitor and welcome monitor from peribot-groupme. While
    these monitors were useful for my own personal Peribot instances, I do
    not feel that they are quite as applicable on a wider scale. In
    particular, as I move toward better support for subscribing to the
    GroupMe Push service, the bot monitor is completely unnecessary and the
    welcome monitor may be somewhat redundant (as group join notifications
    are sent over the push channel).
    
    For my personal use as I work to transition things, I have moved the
    monitors directly into my personal instances. This seems more fitting,
    as they are particular to my instance and it will be easier to modify
    them once they are no longer necessary.

--- lib/peribot/groupme.rb
+++ lib/peribot/groupme.rb
@@ -1,16 +1,10 @@
 require 'peribot'
 require 'groupme'
 
-require 'peribot/groupme/monitor'
-require 'peribot/groupme/bot_monitor'
-require 'peribot/groupme/welcome_monitor'
-
-require 'peribot/groupme/image_processor'
-
 require 'peribot/groupme/bot_sender'
-require 'peribot/groupme/sender'
+require 'peribot/groupme/image_processor'
 require 'peribot/groupme/like_sender'
-
+require 'peribot/groupme/sender'
 require 'peribot/groupme/version'
 
 module Peribot
@@ -19,51 +13,15 @@ module Peribot
   # this work, Peribot::GroupMe provides various components that connect that
   # plumbing to GroupMe's inputs and outputs.
   module GroupMe
-    # The interval between monitor executions, in seconds.
-    MONITOR_INTERVAL = 20
-
-    # The interval after which monitor tasks are considered to have failed, in
-    # seconds.
-    MONITOR_TIMEOUT = 15
-
     module_function
 
     # Register GroupMe components with the given bot. This sets up the bot for
     # communication with GroupMe.
-    def register_into(bot, with_monitors: true)
+    def register_into(bot)
       bot.postprocessor.register Peribot::GroupMe::ImageProcessor
 
       bot.sender.register Peribot::GroupMe::Sender
       bot.sender.register Peribot::GroupMe::LikeSender
-
-      start_monitors_for bot if with_monitors
-    end
-
-    class << self
-      private
-
-      # (private)
-      #
-      # Start the bot and welcome monitors for this instance.
-      def start_monitors_for(bot)
-        timer_args = {
-          execution_interval: MONITOR_INTERVAL,
-          timeout_interval: MONITOR_TIMEOUT,
-          run_now: true
-        }
-
-        Peribot::GroupMe::BotMonitor.start bot, client(bot), **timer_args
-        Peribot::GroupMe::WelcomeMonitor.start bot, client(bot), **timer_args
-      end
-
-      # (private)
-      #
-      # Retrieve a GroupMe client based on the token in a bot's config.
-      def client(bot)
-        ::GroupMe::Client.new token: bot.config['groupme']['token']
-      rescue NoMethodError
-        raise 'could not obtain GroupMe token (does groupme.conf exist?)'
-      end
     end
   end
 end
--- spec/peribot/groupme_spec.rb
+++ spec/peribot/groupme_spec.rb
@@ -17,44 +17,15 @@ describe Peribot::GroupMe do
       allow(bot).to receive(:sender).and_return(sender)
     end
 
-    it 'registers tasks and starts monitors' do
+    it 'registers tasks' do
       expect(bot.postprocessor).to receive(:register)
         .with(Peribot::GroupMe::ImageProcessor)
 
       expect(bot.sender).to receive(:register).with(Peribot::GroupMe::Sender)
       expect(bot.sender).to receive(:register)
         .with(Peribot::GroupMe::LikeSender)
-
-      expect(Peribot::GroupMe::BotMonitor).to receive(:start)
-      expect(Peribot::GroupMe::WelcomeMonitor).to receive(:start)
 
       Peribot::GroupMe.register_into bot
-    end
-
-    it 'allows monitors not to be started' do
-      expect(bot.postprocessor).to receive(:register)
-        .with(Peribot::GroupMe::ImageProcessor)
-
-      expect(bot.sender).to receive(:register).with(Peribot::GroupMe::Sender)
-      expect(bot.sender).to receive(:register)
-        .with(Peribot::GroupMe::LikeSender)
-
-      expect(Peribot::GroupMe::BotMonitor).to_not receive(:start)
-      expect(Peribot::GroupMe::WelcomeMonitor).to_not receive(:start)
-
-      Peribot::GroupMe.register_into bot, with_monitors: false
-    end
-
-    it 'raises an error when the bot is not configured' do
-      allow(bot).to receive(:config).and_return(nil)
-
-      allow(bot.postprocessor).to receive(:register)
-      allow(bot.sender).to receive(:register)
-      allow(Peribot::GroupMe::BotMonitor).to receive(:start)
-      allow(Peribot::GroupMe::WelcomeMonitor).to receive(:start)
-
-      msg = 'could not obtain GroupMe token (does groupme.conf exist?)'
-      expect { Peribot::GroupMe.register_into bot }.to raise_error(msg)
     end
   end
 end
